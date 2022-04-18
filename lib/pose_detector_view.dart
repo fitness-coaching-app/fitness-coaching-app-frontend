071bc3d2c4370b948598e7c3a81980c9a4f635cc
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-04-18 23:54:36 +0700

    :sparkles: Integrate ECM briefly

--- lib/pose_detector_view.dart
+++ lib/pose_detector_view.dart
@@ -6,6 +6,7 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 
 import 'WorkoutLandscapeStepBeginCamera_view.dart';
 import 'WorkoutLandscapeWarnningCamera_view.dart';
+import 'loading_view.dart';
 import 'pose_painter.dart';
 import 'workoutPortraitWarnningCamera_view.dart';
 import 'workoutLandscapeStepCountingCamera_view.dart';
@@ -34,16 +35,20 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       poseDetectorOptions: PoseDetectorOptions(
           model: PoseDetectionModel.base, mode: PoseDetectionMode.streamImage));
   bool isBusy = false;
+  bool isLoaded = false;
   CustomPaint? customPaint;
   late String data;
   late ExerciseController controller;
   late ExerciseState currentState;
 
+  String stepName = "";
   String criteria = "";
   String criteriaValue = "0";
 
   String poseSuggestionString = "";
   int fps = 0;
+
+  late final Future? futureLoadData;
 
   @override
   void dispose() async {
@@ -51,83 +56,92 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
     await poseDetector.close();
   }
 
-  _asyncMethod() async {
-    data = await rootBundle.loadString('assets/jumping-jacks.yaml');
+  Future<String> loadData() async {
+    data = await rootBundle.loadString('assets/yaml/jumping-jacks.yaml');
     controller = ExerciseController(data);
     currentState = controller.getCurrentState();
+    stepName = currentState.stepName;
 
     if (currentState.criteria == ExerciseDisplayCriteria.counter) {
       criteria = "Count";
     } else {
       criteria = "Timer";
     }
+    print("OK...");
+
+    return "OK";
   }
 
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      _asyncMethod();
-    });
+    futureLoadData = loadData();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      WorkoutPortraitStepBeginCamera(
-        title: 'Pose Detector',
-        customPaint: customPaint,
-        onImage: (inputImage) {
-          processImage(inputImage);
-        },
-      ),
-      Positioned(
-          bottom: 30,
-          left: 20,
-          child: Row(children: <Widget>[
-            TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.blue,
-                  backgroundColor: Colors.white,
-                ),
-                onPressed: () {
-                  controller.dumpLogToFile(
-                      "squats-10_${DateTime.now().toUtc().toString()}");
+    return FutureBuilder(
+        future: futureLoadData,
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (!snap.hasData) {
+            return Loading();
+          } else {
+            return Stack(children: <Widget>[
+              WorkoutPortraitStepBeginCamera(
+                title: 'Pose Detector',
+                customPaint: customPaint,
+                onImage: (inputImage) {
+                  processImage(inputImage);
                 },
-                child: Text("Save to Log")),
-            SizedBox(width: 25),
-            TextButton(
-                style: TextButton.styleFrom(
-                  primary: Colors.white,
-                  backgroundColor: Colors.red,
-                ),
-                onPressed: () {
-                  // controller.dumpLogToFile('test');
-                },
-                child: Text("Clear Log"))
-          ])),
-      Positioned(
-        top: 100,
-        left: 20,
-        child: Column(children: <Widget>[
-          Text("$criteria: $criteriaValue\nSuggestion: $poseSuggestionString",
-              style: TextStyle(
-                backgroundColor: Colors.black,
-                color: Colors.white,
-                fontSize: 12
-              ))
-        ]),
-      ),
-      Positioned(
-          top: 130,
-          right: 20,
-          child: Text("$fps fps",
-              style: TextStyle(
-                backgroundColor: Colors.black,
-                color: Colors.white,
-                fontSize: 12
-              )))
-    ]);
+              ),
+              Positioned(
+                  bottom: 30,
+                  left: 20,
+                  child: Row(children: <Widget>[
+                    TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.blue,
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: () {
+                          controller.dumpLogToFile(
+                              "squats-10_${DateTime.now().toUtc().toString()}");
+                        },
+                        child: Text("Save to Log")),
+                    SizedBox(width: 25),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                          primary: Colors.white,
+                          backgroundColor: Colors.red,
+                        ),
+                        onPressed: () {
+                          // controller.dumpLogToFile('test');
+                        },
+                        child: Text("Clear Log"))
+                  ])),
+              Positioned(
+                top: 100,
+                left: 20,
+                child: Column(children: <Widget>[
+                  Text(
+                      "$stepName\n$criteria: $criteriaValue\nSuggestion: $poseSuggestionString",
+                      style: TextStyle(
+                          backgroundColor: Colors.black,
+                          color: Colors.white,
+                          fontSize: 12))
+                ]),
+              ),
+              Positioned(
+                  top: 130,
+                  right: 20,
+                  child: Text("$fps fps",
+                      style: TextStyle(
+                          backgroundColor: Colors.black,
+                          color: Colors.white,
+                          fontSize: 12)))
+            ]);
+          }
+        });
   }
 
   Future<void> processImage(InputImage inputImage) async {
@@ -152,18 +166,19 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       setState(() {});
     }
     final DateTime stopTime = DateTime.now();
-    setState((){
+    setState(() {
       fps = ((1 / stopTime.difference(time).inMilliseconds) * 1000).toInt();
     });
     // print(
     //     "Process time: ${stopTime.difference(time).inMilliseconds} ms | ${detectionTime.difference(time).inMicroseconds} us | ${poseProcessorTime.difference(detectionTime).inMicroseconds} us");
   }
 
-    Future<void> poseProcessorTest(Pose pose) async {
+  Future<void> poseProcessorTest(Pose pose) async {
     controller.setPose(pose);
     controller.update();
     setState(() {
-      poseSuggestionString = controller.getCurrentState().getWarning()["warningMessage"];
+      poseSuggestionString =
+          controller.getCurrentState().getWarning()["warningMessage"];
       if (controller.getCurrentState().criteria ==
           ExerciseDisplayCriteria.counter) {
         criteria = "Count";
@@ -176,62 +191,61 @@ class _PoseDetectorViewState extends State<PoseDetectorView> {
       }
     });
   }
-
 }
 
-  // Future<void> processImage(InputImage inputImage) async {
-  //   if (isBusy) return;
-  //   isBusy = true;
-  //   final poses = await poseDetector.processImage(inputImage);
-  //   print('Found ${poses.length} poses');
-  //   for (Pose pose in poses) {
-      // angle
-      // PoseValidator poseValidator = PoseValidator(pose);
-      // armAngle = poseValidator.getAngle(PoseLandmarkType.leftShoulder,
-      //     PoseLandmarkType.leftHip, PoseLandmarkType.leftElbow);
-      // elbowAngle = poseValidator.getAngle(PoseLandmarkType.leftElbow,
-      //     PoseLandmarkType.leftShoulder, PoseLandmarkType.leftWrist);
-      // print('arm = ' + armAngle.toString());
-      // print('elbow = ' + elbowAngle.toString());
+// Future<void> processImage(InputImage inputImage) async {
+//   if (isBusy) return;
+//   isBusy = true;
+//   final poses = await poseDetector.processImage(inputImage);
+//   print('Found ${poses.length} poses');
+//   for (Pose pose in poses) {
+// angle
+// PoseValidator poseValidator = PoseValidator(pose);
+// armAngle = poseValidator.getAngle(PoseLandmarkType.leftShoulder,
+//     PoseLandmarkType.leftHip, PoseLandmarkType.leftElbow);
+// elbowAngle = poseValidator.getAngle(PoseLandmarkType.leftElbow,
+//     PoseLandmarkType.leftShoulder, PoseLandmarkType.leftWrist);
+// print('arm = ' + armAngle.toString());
+// print('elbow = ' + elbowAngle.toString());
 
-      //touch checker
-      // TouchChecker touchChecker = TouchChecker();
-      // touchChecker.setPose(pose);
-      // var checkLeftRightHand = touchChecker.touchChecker(
-      //     PoseLandmarkType.leftWrist, PoseLandmarkType.rightWrist);
-      // print(
-      //     'check left and right wrist touch: ' + checkLeftRightHand.toString());
+//touch checker
+// TouchChecker touchChecker = TouchChecker();
+// touchChecker.setPose(pose);
+// var checkLeftRightHand = touchChecker.touchChecker(
+//     PoseLandmarkType.leftWrist, PoseLandmarkType.rightWrist);
+// print(
+//     'check left and right wrist touch: ' + checkLeftRightHand.toString());
 
-      // pose.landmarks.forEach((_, landmark) {
-      //   final type = landmark.type;
-      //   final x = landmark.x;
-      //   final y = landmark.y;
-      //   final z = landmark.z;
-      //   final likelihood = landmark.likelihood;
-      // print('$type: PoseLandmark($type, $x, $y, $z, $likelihood)');
-      // print('$type: PoseLandmark($type, likelihood: $likelihood)');
-      // if (inputImage.inputImageData?.size != null &&
-      //     inputImage.inputImageData?.imageRotation != null) {
-      //   // final painter = PosePainter(poses, inputImage.inputImageData!.size,
-      //   //     inputImage.inputImageData!.imageRotation, armAngle, elbowAngle);
-      //   final painter = PosePainter(poses, inputImage.inputImageData!.size,
-      //       inputImage.inputImageData!.imageRotation);
-      //   customPaint = CustomPaint(painter: painter);
-      // } else {
-      //   customPaint = null;
-      // }
-      // });
-    // }
-    // print('Found ${poses.length} poses');
-    // if (inputImage.inputImageData?.size != null &&
-    //     inputImage.inputImageData?.imageRotation != null) {
-    //   final painter = PosePainter(poses, inputImage.inputImageData!.size,
-    //       inputImage.inputImageData!.imageRotation);
-    //   customPaint = CustomPaint(painter: painter);
+// pose.landmarks.forEach((_, landmark) {
+//   final type = landmark.type;
+//   final x = landmark.x;
+//   final y = landmark.y;
+//   final z = landmark.z;
+//   final likelihood = landmark.likelihood;
+// print('$type: PoseLandmark($type, $x, $y, $z, $likelihood)');
+// print('$type: PoseLandmark($type, likelihood: $likelihood)');
+// if (inputImage.inputImageData?.size != null &&
+//     inputImage.inputImageData?.imageRotation != null) {
+//   // final painter = PosePainter(poses, inputImage.inputImageData!.size,
+//   //     inputImage.inputImageData!.imageRotation, armAngle, elbowAngle);
+//   final painter = PosePainter(poses, inputImage.inputImageData!.size,
+//       inputImage.inputImageData!.imageRotation);
+//   customPaint = CustomPaint(painter: painter);
+// } else {
+//   customPaint = null;
+// }
+// });
+// }
+// print('Found ${poses.length} poses');
+// if (inputImage.inputImageData?.size != null &&
+//     inputImage.inputImageData?.imageRotation != null) {
+//   final painter = PosePainter(poses, inputImage.inputImageData!.size,
+//       inputImage.inputImageData!.imageRotation);
+//   customPaint = CustomPaint(painter: painter);
 
-    // } else {
-    //   customPaint = null;
-    // }
+// } else {
+//   customPaint = null;
+// }
 //     isBusy = false;
 //     if (mounted) {
 //       setState(() {});
